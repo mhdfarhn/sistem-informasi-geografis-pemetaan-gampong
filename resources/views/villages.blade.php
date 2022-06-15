844b74b7096348b86d4dc370b2c8158d0a138162
Muhammad Farhan <mhdfarhn@gmail.com>
2022-06-15 20:28:43 +0700

    add custom button for draw dictrict boundaries

--- resources/views/villages.blade.php
+++ resources/views/villages.blade.php
@@ -10,7 +10,16 @@
     </div>
 
     {{-- Maps --}}
-    <div id="map" style="height: 400px;" class="mb-4"></div>
+    {{-- <div class="row"> --}}
+      <div id="map" style="height: 400px;" class="mb-4 position-relative"></div>
+      {{-- <div style="height: 400px;" class="col-2 border text-center">
+        <h6 class="text-center mt-3">Pemetaan Gampong</h6>
+        <hr>
+        <div id="listings"></div>
+        <button type="button" class="btn btn-dark" id="baro">Gampong Baro</button>
+      </div> --}}
+      {{--
+    </div> --}}
 
     <hr>
 
@@ -164,6 +173,276 @@
   loadLocations(customData)
 
   map.addControl(geocoder)
+
+  // // Baro
+  // map.addSource('baro', {
+  //   'type': 'geojson',
+  //   'data': {
+  //     'type': 'Feature',
+  //     'geometry': {
+  //       'type': 'Polygon',
+  //       // These coordinates outline Maine.
+  //       'coordinates': [
+  //         [
+  //           [97.97622985621754, 4.460672491919002],
+  //           [97.97710059456669, 4.461838050379202],
+  //           [97.97730798703785, 4.4619523786765205],
+  //           [97.9791870498509, 4.46451043753472],
+  //           [97.98153848071826, 4.462733883494449],
+  //           [97.98145915215672, 4.462621335098518],
+  //           [97.98190773816478, 4.4623850086242705],
+  //           [97.98200969069576, 4.462497945819029],
+  //           [97.98279982281072, 4.461989728305845],
+  //           [97.98275451057472, 4.461820322389938],
+  //           [97.9834341942394, 4.461422218320832],
+  //           [97.98291593548585, 4.460583657980855],
+  //           [97.98114592620475, 4.460329548605982],
+  //           [97.98067014772694, 4.460414251740762],
+  //           [97.98053137900423, 4.460507425177743],
+  //           [97.98054270706324, 4.460501778303103],
+  //           [97.98025100943573, 4.4598072123810075],
+  //           [97.97917201168303, 4.4599540312515815],
+  //           [97.97824311084537, 4.460363429855159],
+  //           [97.97801654966545, 4.460052851625059],
+  //           [97.97778149232835, 4.460131907922093],
+  //           [97.97768520382688, 4.459993559410385],
+  //           [97.97743315451422, 4.460261786093049],
+  //           [97.97622954809009, 4.460668361095953]
+  //         ]
+  //       ]
+  //     }
+  //   }
+  // });
+  
+  // function btnBaro() {
+  //   // Add a new layer to visualize the polygon.
+  //   map.addLayer({
+  //     'id': 'maine',
+  //     'type': 'fill',
+  //     'source': 'maine', // reference the data source
+  //     'layout': {},
+  //     'paint': {
+  //       'fill-color': '#0080ff', // blue color fill
+  //       'fill-opacity': 0.5
+  //     }
+  //   });
+  //   // Add a black outline around the polygon.
+  //   map.addLayer({
+  //     'id': 'outline',
+  //     'type': 'line',
+  //     'source': 'maine',
+  //     'layout': {},
+  //     'paint': {
+  //       'line-color': '#000',
+  //       'line-width': 3
+  //     }
+  //   });
+  // }
+
+  // const element = document.getElementById("baro")
+  // element.addEventListener("click", btnBaro)
+  
+  // customData.features.forEach((data, i) => {
+  //   data.properties.id = i;
+  // });
+
+  // map.on('load', () => {
+  //   // map.addSource('places', {
+  //   //   'type': 'geojson',
+  //   //   'data': customData
+  //   // })
+  //   map.addSource('gpbaro', {
+  //     'type': 'geojson',
+  //     'data': {
+  //       'type': 'Feature',
+  //       'geometry': {
+  //         'type': 'Polygon',
+  //         // These coordinates outline Maine.
+  //         'coordinates': [
+  //           [
+  //             [97.97622985621754, 4.460672491919002],
+  //             [97.97710059456669, 4.461838050379202],
+  //             [97.97730798703785, 4.4619523786765205],
+  //             [97.9791870498509, 4.46451043753472],
+  //             [97.98153848071826, 4.462733883494449],
+  //             [97.98145915215672, 4.462621335098518],
+  //             [97.98190773816478, 4.4623850086242705],
+  //             [97.98200969069576, 4.462497945819029],
+  //             [97.98279982281072, 4.461989728305845],
+  //             [97.98275451057472, 4.461820322389938],
+  //             [97.9834341942394, 4.461422218320832],
+  //             [97.98291593548585, 4.460583657980855],
+  //             [97.98114592620475, 4.460329548605982],
+  //             [97.98067014772694, 4.460414251740762],
+  //             [97.98053137900423, 4.460507425177743],
+  //             [97.98054270706324, 4.460501778303103],
+  //             [97.98025100943573, 4.4598072123810075],
+  //             [97.97917201168303, 4.4599540312515815],
+  //             [97.97824311084537, 4.460363429855159],
+  //             [97.97801654966545, 4.460052851625059],
+  //             [97.97778149232835, 4.460131907922093],
+  //             [97.97768520382688, 4.459993559410385],
+  //             [97.97743315451422, 4.460261786093049],
+  //             [97.97622954809009, 4.460668361095953]
+  //           ]
+  //         ]
+  //       }
+  //     }
+  //   });
+
+  //   // buildLocationList(customData)
+
+  //   const btnBaro = document.getElementById('baro')
+
+  //   btnBaro.addEventListener('click', function() {
+  //     map.addLayer({
+  //       'id': 'gpbaro',
+  //       'type': 'fill',
+  //       'source': 'gpbaro', // reference the data source
+  //       'layout': {},
+  //       'paint': {
+  //         'fill-color': '#0080ff', // blue color fill
+  //         'fill-opacity': 0.5
+  //       }
+  //     });
+  //     // Add a black outline around the polygon.
+  //     map.addLayer({
+  //       'id': 'outline',
+  //       'type': 'line',
+  //       'source': 'gpbaro',
+  //       'layout': {},
+  //       'paint': {
+  //         'line-color': '#000',
+  //         'line-width': 3
+  //       }
+  //     });
+  //   })
+  // })
+
+  // function buildLocationList(customData) {
+  //   for (const data of customData.features) {
+  //     const listings = document.getElementById('listings')
+  //     const listing = listings.appendChild(document.createElement('div'))
+
+  //     listing.id = `listing-${data.properties.id}`
+  //     listing.className = 'item'
+  //     const link = listing.appendChild(document.createElement('a'));
+  //     link.href = '#';
+  //     link.id = `link-${data.properties.id}`;
+  //     link.innerHTML = `${data.properties.name}`;
+
+  //     link.addEventListener('click', function() {
+  //       for (const feature of customData.features) {
+  //         if (this.id === `link.${feature.properties.id}`) {
+  //           flyToStore(feature)
+  //         }
+  //       }
+  //     })
+  //   }
+  // }
+
+  // function flyToStore(currentFeature) {
+  //   map.flyTo({
+  //     center: currentFeature.geometry.coordinates,
+  //     zoom: 15
+  //   });
+  // }
+
+  const myCustomControl = new MyCustomControl();
+
   map.addControl(new mapboxgl.NavigationControl())
+  map.addControl(myCustomControl);
+
+  class MyCustomControl {
+    onAdd(map){
+      this.map = map;
+      this.container = document.createElement('div');
+      this.container.className = 'my-custom-control';
+      const button = this._createButton('pemetaan')
+      this.container.appendChild(button)
+      return this.container;
+    }
+
+    onRemove(){
+      this.container.parentNode.removeChild(this.container);
+      this.map = undefined;
+    }
+    
+    _createButton(className) {
+      const el = window.document.createElement('button')
+      el.className = className;
+      el.id = 'buttonPeta';
+      el.textContent = 'Pemetaan';
+      el.addEventListener('click', (e) => {
+        // console.log('tes');
+        drawPolygon(e);
+      }, false )
+      return el;
+    }
+
+    // function drawPolygon(e) {
+    //   map.on('load', () => {
+    //   map.addSource('gpbaro', {
+    //     'type': 'geojson',
+    //     'data': {
+    //       'type': 'Feature',
+    //       'geometry': {
+    //         'type': 'Polygon',
+    //         'coordinates': [
+    //           [
+    //             [97.97622985621754, 4.460672491919002],
+    //             [97.97710059456669, 4.461838050379202],
+    //             [97.97730798703785, 4.4619523786765205],
+    //             [97.9791870498509, 4.46451043753472],
+    //             [97.98153848071826, 4.462733883494449],
+    //             [97.98145915215672, 4.462621335098518],
+    //             [97.98190773816478, 4.4623850086242705],
+    //             [97.98200969069576, 4.462497945819029],
+    //             [97.98279982281072, 4.461989728305845],
+    //             [97.98275451057472, 4.461820322389938],
+    //             [97.9834341942394, 4.461422218320832],
+    //             [97.98291593548585, 4.460583657980855],
+    //             [97.98114592620475, 4.460329548605982],
+    //             [97.98067014772694, 4.460414251740762],
+    //             [97.98053137900423, 4.460507425177743],
+    //             [97.98054270706324, 4.460501778303103],
+    //             [97.98025100943573, 4.4598072123810075],
+    //             [97.97917201168303, 4.4599540312515815],
+    //             [97.97824311084537, 4.460363429855159],
+    //             [97.97801654966545, 4.460052851625059],
+    //             [97.97778149232835, 4.460131907922093],
+    //             [97.97768520382688, 4.459993559410385],
+    //             [97.97743315451422, 4.460261786093049],
+    //             [97.97622954809009, 4.460668361095953]
+    //           ]
+    //         ]
+    //       }
+    //     }
+    //   });
+
+    //   map.addLayer({
+    //     'id': 'gpbaro',
+    //     'type': 'fill',
+    //     'source': 'gpbaro', // reference the data source
+    //     'layout': {},
+    //     'paint': {
+    //       'fill-color': '#0080ff', // blue color fill
+    //       'fill-opacity': 0.5
+    //     }
+    //   });
+
+    //   map.addLayer({
+    //     'id': 'outline',
+    //     'type': 'line',
+    //     'source': 'gpbaro',
+    //     'layout': {},
+    //     'paint': {
+    //       'line-color': '#000',
+    //       'line-width': 3
+    //     }
+    //   });
+    //   })
+    // }
+  }
 </script>
 @endpush
